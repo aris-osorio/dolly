256042bae4d4253fbc93f50aa125047e5090b68c
aris-osorio <aris.osorio@alumnos.udg.mx>
2020-12-17 01:30:34 -0600

    add uuid

--- dolly/tarjetas/models.py
+++ dolly/tarjetas/models.py
@@ -1,9 +1,11 @@
+import uuid
 from django.db import models
 from listas.models import Lista
 from usuarios.models import Usuario
 
 # Create your models here.
 class Tarjeta(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     nombre = models.CharField(max_length = 200)
     lista = models.ForeignKey(Lista, on_delete = models.CASCADE)
     descripcion = models.CharField(max_length = 200)
@@ -11,7 +13,7 @@ class Tarjeta(models.Model):
     dueño = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_vencimiento = models.DateTimeField(null = True)
-    posicion = models.IntegerField()
+    posicion = models.IntegerField(unique= True)
 
     def __str__(self):
         return self.nombre
